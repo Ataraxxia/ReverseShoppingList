94dabfd76e13dcb5d921376d087b8c4fc6dbe4d4
Ataraxxia <ataraxxia@com>
2018-02-06 15:55:32 +0100

    Added tabbed activity

--- app/src/main/java/com/example/tai/reverseshoppinglist/MainActivity.kt
+++ app/src/main/java/com/example/tai/reverseshoppinglist/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.tai.reverseshoppinglist
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
@@ -7,11 +8,11 @@ import android.view.Window
 import android.widget.Toast
 import android.view.WindowManager
 import android.os.Build
-
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,15 +20,16 @@ class MainActivity : AppCompatActivity() {
             val w = window
             w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         }
-
         setContentView(R.layout.activity_main)
 
 
         val myFab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
 
         myFab.setOnClickListener {
-            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, AddActivity::class.java).apply {}
+            startActivity(intent)
         }
 
     }
+
 }
