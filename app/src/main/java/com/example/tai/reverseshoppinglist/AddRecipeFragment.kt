65b109fc62e9ad5ed74821da752ef19e6c43c5d2
Ataraxxia <ataraxxia@com>
2018-02-07 23:01:14 +0100

    Add recipie ui changes

--- app/src/main/java/com/example/tai/reverseshoppinglist/AddRecipeFragment.kt
+++ app/src/main/java/com/example/tai/reverseshoppinglist/AddRecipeFragment.kt
@@ -5,14 +5,22 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.LinearLayout
 
-/**
- * Created by tai on 2/6/18.
- */
+
 class AddRecipeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater!!.inflate(R.layout.addrecipe_fragment, container, false)
+
+        val linearLayout = view.findViewById<LinearLayout>(R.id.linearLayout) as LinearLayout
+
+        val recipeName = EditText(context)
+        recipeName.hint = "Recipe name"
+        recipeName.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+
+        linearLayout.addView(recipeName)
 
         return view
     }
